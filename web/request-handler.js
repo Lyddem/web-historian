e7b36a1f9e635e92b958dc301d41625904fc28a7
Lyddem <lydiademi@gmail.com>
2017-09-15 19:55:07 -0700

    First Commit

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,17 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
-
+var userInterface = fs.readFileSync('./public/index.html');
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  res.end(userInterface);
 };
+
+//serve HTML file
+
+//GET request (return content of a website from file)
+
+//404 if non-existent file
+
+//make a post to server
